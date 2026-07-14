68a2f872d956026894c4fe6a60f5bade7f32c794
Martin Gäckler <git@gaeckler.at>
2026-07-14 18:23:24 +0200

    Update Plotter.cpp

--- Plotter.cpp
+++ Plotter.cpp
@@ -127,7 +127,7 @@ class WindowsApplication : public GuiApplication
 	}
 	virtual CallbackWindow  *createMainWindow( const char * /*cmdLine*/, int /*nCmdShow*/ )
 	{
-		std::auto_ptr<PlotterMainWindow>	mainWindow( new PlotterMainWindow );
+		std::unique_ptr<PlotterMainWindow>	mainWindow( new PlotterMainWindow );
 		if( mainWindow->create( nullptr ) == scERROR )
 		{
 			throw gak::LibraryException( "Could not create window!" );
